b836b3368eccebc24ff1e6868a8764595d2380d4
Johanced <johan.cederhag@hotmail.se>
2021-11-24 21:47:02 +0100

    Changed folders

--- Laboration2/src/View.java
+++ Laboration2/src/View.java
@@ -21,8 +21,7 @@ public class View implements ActionListener, DocumentListener {
 	private Controller controller;
 
 	public View(Controller c) {
-		this.controller = c;
-		
+		this.controller = c;	
 		initGUI();
 	}
 	
